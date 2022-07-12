010937518a62b34fa663660982f89357219f5c50
merraa <meron12345@yahoo.com>
2022-07-12 02:16:22 -0700

    everything in one go

---
+++ _printf.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <unistd.h>
+
+/**
+ * _printf - prints a formatted string
+ * @format: string to print
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i, vacount = 0;
+	const char *str = format;
+	char c;
+	va_list valist;
+
+	if (format == NULL)
+		return (0);
+	va_start(valist, format);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		while (str[i] != '%')
+		{
+			_putchar(str[i]);
+			i++;
+		}
+		c = str[i++];
+		if (c == '\0' || c == '%')
+		{
+			_putchar('%');
+		}
+		func_print(c, valist);
+		i++;
+		vacount++;
+	}
+	va_end(valist);
+	return (i - vacount);
+}
---
+++ main.h
@@ -0,0 +1,31 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct conv_spec - struct containing identifiers and their
+ * conversion specifiers
+ *@id: id character
+ * @func: function pointer to correct conversion function
+ */
+
+typedef struct conv_spec
+{
+	char id;
+	int (*func)(va_list);
+} spec;
+
+int _printf(const char *format, ...);
+int func_print(char k, va_list valist):
+int print_d(va_list valist);
+int print_i(va_list valist);
+int print_s(va_list valist);
+void rev_buff(char *s);
+int _putchar(char c);
+int _itoa(int z);
+
+#endif
